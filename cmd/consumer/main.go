c70afb1ba48607d771f2c6ae8f66a5af6ee23b8d
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-23 19:13:33 -0300

    reading kafka messages

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "kafka:9092",
+		"client.id":         "goapp-consumer",
+		"group.id":          "goapp-group",
+		"auto.offset.reset": "earliest",
+	}
+
+	c, err := kafka.NewConsumer(configMap)
+	if err != nil {
+		fmt.Println("error consumer: ", err.Error())
+	}
+
+	topics := []string{"teste"}
+	c.SubscribeTopics(topics, nil)
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, msg.Value)
+		}
+	}
 }
