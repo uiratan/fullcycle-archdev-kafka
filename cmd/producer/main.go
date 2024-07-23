aa588e4b93984c26aa1010ecc4976bd122adab51
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-23 18:50:31 -0300

    getting async ack with goroutines

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
@@ -11,21 +12,26 @@ func main() {
 	producer := NewKafkaProducer()
 	Publish("Hello Kafka!", "teste", producer, nil, deliveryChan)
 
-	e := <-deliveryChan
-	msg := e.(*kafka.Message)
+	go DeliveryReport(deliveryChan) // async
 
-	if msg.TopicPartition.Error != nil {
-		log.Println("Erro ao enviar: ", msg.TopicPartition.Error)
-	} else {
-		log.Println("Mensagem enviada: ", msg.TopicPartition)
-	}
+	fmt.Println("Mensagem enviada com sucesso!")
+	// e := <-deliveryChan
+	// msg := e.(*kafka.Message)
 
+	// if msg.TopicPartition.Error != nil {
+	// 	log.Println("Erro ao enviar: ", msg.TopicPartition.Error)
+	// } else {
+	// 	log.Println("Mensagem enviada: ", msg.TopicPartition)
+	// }
 	producer.Flush(1000)
 }
 
 func NewKafkaProducer() *kafka.Producer {
 	configMap := &kafka.ConfigMap{
-		"bootstrap.servers": "kafka:9092",
+		"bootstrap.servers":   "kafka:9092",
+		"delivery.timeout.ms": "0",
+		"acks":                "all",  // 0 nao precisa receber retorno, 1 aguarda que pelo menos um leader respondeu, all leader e brokers respondeem
+		"enable.idempotence":  "true", // false por padrao, se for true o acks tem que ser all
 	}
 
 	p, err := kafka.NewProducer(configMap)
@@ -48,4 +54,19 @@ func Publish(msg string, topic string, producer *kafka.Producer, key []byte, del
 		return err
 	}
 	return nil
+}
+
+func DeliveryReport(deliveryChan chan kafka.Event) {
+	for e := range deliveryChan {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				log.Println("Erro ao enviar: ", ev.TopicPartition)
+			} else {
+				log.Println("Mensagem enviada: ", ev.TopicPartition)
+				// anotar no bd que a msg foi enviada
+				// ex: confirma que uma transação bancaria ocorreu
+			}
+		}
+	}
 }
